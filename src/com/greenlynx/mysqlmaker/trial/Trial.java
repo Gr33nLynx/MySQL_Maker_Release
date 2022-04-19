ffe72bd315bdcf4e8e9ade4d70251c3f05c0758a
gr33nlynx <greenlynxonfire@gmail.com>
2022-04-19 16:47:37 +0200

    Remove unused import

--- src/com/greenlynx/mysqlmaker/trial/Trial.java
+++ src/com/greenlynx/mysqlmaker/trial/Trial.java
@@ -29,7 +29,6 @@ import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
