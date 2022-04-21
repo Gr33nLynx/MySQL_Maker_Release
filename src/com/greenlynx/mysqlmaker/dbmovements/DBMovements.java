805308ac94e2eb75fdfb21fc57dc2bd28e464b31
gr33nlynx <greenlynxonfire@gmail.com>
2022-04-21 16:59:12 +0200

    Created DBMovements.java class

---
+++ src/com/greenlynx/mysqlmaker/dbmovements/DBMovements.java
@@ -0,0 +1,50 @@
+package com.greenlynx.mysqlmaker.dbmovements;
+
+/**
+ * <b>PROJECT NAME:</b><br> 
+ * &nbsp;&nbsp;&nbsp;&nbsp;MySQL Maker<br>
+ * <b>PROJECT DESCRIPTION:</b><br>
+ * &nbsp;&nbsp;&nbsp;&nbsp;GUI for the creation of MySQL code in an automated way<br>
+ * <b>FILE NAME:</b><br>
+ * &nbsp;&nbsp;&nbsp;&nbsp;DBMovements.java<br>
+ * <b>FILE DESCRIPTION:</b><br>
+ * &nbsp;&nbsp;&nbsp;&nbsp;DB Movements Matters Class<br>
+ * <b>NOTES:</b><br>
+ * &nbsp;&nbsp;&nbsp;&nbsp;All plagiarism attempts // Code theft will be reported by the 
+ * &nbsp;&nbsp;&nbsp;&nbsp;author<br>
+ * <b>COPYRIGHT:</b><br>
+ * &nbsp;&nbsp;&nbsp;&nbsp;©2022 GR33NLYNX
+ * @author GR33NLYNX
+ * @version 1.0
+ */
+public class DBMovements {
+
+	public static void InsertQuery() {
+		
+	}
+	
+	public static void SelectQuery() {
+			
+		}
+	
+	public static void UpdateQuery() {
+		
+	}
+	
+	public static void DeleteQuery() {
+		
+	}
+	
+	public static void Procedure() {
+		
+	}
+	
+	public static void Function() {
+		
+	}
+	
+	public static void Trigger() {
+		
+	}
+	
+}
