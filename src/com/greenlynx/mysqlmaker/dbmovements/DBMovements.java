4631d5c184bc640f4d83af1a32151432c0168be8
gr33nlynx <greenlynxonfire@gmail.com>
2022-05-04 18:43:46 +0200

    Update DBMovements class

--- src/com/greenlynx/mysqlmaker/dbmovements/DBMovements.java
+++ src/com/greenlynx/mysqlmaker/dbmovements/DBMovements.java
@@ -1,14 +1,21 @@
 package com.greenlynx.mysqlmaker.dbmovements;
 
+// ============= Imports =============
+import com.greenlynx.mysqlmaker.dbstatus.DBStatus;
+import com.greenlynx.mysqlmaker.helpers.Helper_Methods;
 import java.sql.CallableStatement;
 import java.sql.SQLException;
 
-import com.greenlynx.mysqlmaker.dbstatus.DBStatus;
-// ============= Imports =============
-import com.greenlynx.mysqlmaker.helpers.Helper_Methods;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 // ===================================
 
@@ -33,14 +40,33 @@ public class DBMovements {
 
 	// ======= Attributes =======
 	public static BorderPane insertPanel;
+	public static Button btnInsertExecution;
+	public static HBox hFields;
+	public static HBox hInsertExecution;
+	public static HBox hTable;
+	public static HBox hValues;
+	public static Label lblFieldsToFill;
+	public static Label lblTableToInsert;
+	public static Label lblValuesToInsert;
 	public static Scene insertScene;
 	public static Stage insertStage;
+	public static TextField txtFieldsToFill;
+	public static TextField txtTableToInsert;
+	public static TextField txtValuesToInsert;
+	public static VBox vInsertBox;
 	
 	public static BorderPane updatePanel;
+	public static Label lblConditionToApplyUpdate;
+	public static Label lblNewValue;
+	public static Label lblTableToUpdate;
+	public static Label lblVariableToUpdate;
 	public static Scene updateScene;
 	public static Stage updateStage;
 	
 	public static BorderPane selectPanel;
+	public static Label lblConditionToApplySelect;
+	public static Label lblDataToRecover;
+	public static Label lblTableToRecover;
 	public static Scene selectScene;
 	public static Stage selectStage;
 	
@@ -65,12 +91,52 @@ public class DBMovements {
 		insertStage = new Stage();
 		insertPanel = new BorderPane();
 		insertScene = new Scene(insertPanel, 400, 300);
+		
+		vInsertBox = new VBox();
+		
+		hTable = new HBox();
+		lblTableToInsert = new Label("Table Name:");
+		lblTableToInsert.setId("lblTableToInsert");
+		txtTableToInsert = new TextField();
+		hTable.setAlignment(Pos.CENTER);
+		hTable.setSpacing(10);
+		hTable.getChildren().addAll(lblTableToInsert, txtTableToInsert);
+		
+		hFields = new HBox();
+		lblFieldsToFill = new Label("Fields:");
+		lblFieldsToFill.setId("lblFieldsToFill");
+		txtFieldsToFill = new TextField();
+		hFields.setAlignment(Pos.CENTER);
+		hFields.setPadding(new Insets(0, 0, 0, 47));
+		hFields.setSpacing(10);
+		hFields.getChildren().addAll(lblFieldsToFill, txtFieldsToFill);
+		
+		hValues = new HBox();
+		lblValuesToInsert = new Label("Values:");
+		lblValuesToInsert.setId("lblValuesToInsert");
+		txtValuesToInsert = new TextField();
+		hValues.setAlignment(Pos.CENTER);
+		hValues.setPadding(new Insets(0, 0, 0, 41));
+		hValues.setSpacing(10);
+		hValues.getChildren().addAll(lblValuesToInsert, txtValuesToInsert);
+		
+		hInsertExecution = new HBox();
+		btnInsertExecution = new Button("Execution!");
+		btnInsertExecution.setOnMouseClicked(e -> InsertQueryAction());
+		hInsertExecution.setAlignment(Pos.CENTER);
+		hInsertExecution.setPadding(new Insets(20, 0, 0, 0));
+		hInsertExecution.getChildren().addAll(btnInsertExecution);
+		
+		vInsertBox.setAlignment(Pos.CENTER);
+		vInsertBox.getChildren().addAll(hTable, hFields, hValues, hInsertExecution);
+		
+		insertPanel.setCenter(vInsertBox);
+		
 		insertStage.setTitle("Insert Query Window");
 		insertStage.setScene(insertScene);
 		insertStage.setResizable(false);
 		insertStage.getIcons().add(new Image("file:res/DBMovements_Insert.png"));
-		insertScene.getStylesheets().add("file:styles/DBMovements_Insert.css");
-		//insertStage.setOnShowing(e -> InsertQueryAction());
+		insertScene.getStylesheets().add("file:styles/DBMovements.css");
 		insertStage.show();
 		Helper_Methods.CenterStage(insertStage);
 	}
@@ -80,6 +146,8 @@ public class DBMovements {
 			CallableStatement statement = DBStatus.DBLink.prepareCall("{ CALL sp_Insert (?, ?, ?) }");
 			
 			statement.execute();
+			
+			insertStage.close();
 		} 
 		catch (SQLException e) {
 			e.printStackTrace();
@@ -95,7 +163,6 @@ public class DBMovements {
 		selectStage.setScene(selectScene);
 		selectStage.setResizable(false);
 		selectStage.getIcons().add(new Image("file:res/DBMovements_Select.png"));
-		selectScene.getStylesheets().add("file:styles/DBMovements_Select.css");
 		selectStage.show();
 		Helper_Methods.CenterStage(selectStage);
 	}
@@ -119,7 +186,6 @@ public class DBMovements {
 		updateStage.setScene(updateScene);
 		updateStage.setResizable(false);
 		updateStage.getIcons().add(new Image("file:res/DBMovements_Update.png"));
-		updateScene.getStylesheets().add("file:styles/DBMovements_Update.css");
 		updateStage.show();
 		Helper_Methods.CenterStage(updateStage);
 	}
@@ -143,7 +209,6 @@ public class DBMovements {
 		deleteStage.setScene(deleteScene);
 		deleteStage.setResizable(false);
 		deleteStage.getIcons().add(new Image("file:res/DBMovements_Delete.png"));
-		deleteScene.getStylesheets().add("file:styles/DBMovements_Delete.css");
 		deleteStage.show();
 		Helper_Methods.CenterStage(deleteStage);
 	}
@@ -167,7 +232,6 @@ public class DBMovements {
 		procedureStage.setScene(procedureScene);
 		procedureStage.setResizable(false);
 		procedureStage.getIcons().add(new Image("file:res/DBMovements_Procedure.png"));
-		procedureScene.getStylesheets().add("file:styles/DBMovements_Procedure.css");
 		procedureStage.show();
 		Helper_Methods.CenterStage(procedureStage);
 	}
@@ -184,7 +248,6 @@ public class DBMovements {
 		functionStage.setScene(functionScene);
 		functionStage.setResizable(false);
 		functionStage.getIcons().add(new Image("file:res/DBMovements_Function.png"));
-		functionScene.getStylesheets().add("file:styles/DBMovements_Function.css");
 		functionStage.show();
 		Helper_Methods.CenterStage(functionStage);
 	}
@@ -201,7 +264,6 @@ public class DBMovements {
 		triggerStage.setScene(triggerScene);
 		triggerStage.setResizable(false);
 		triggerStage.getIcons().add(new Image("file:res/DBMovements_Trigger.png"));
-		triggerScene.getStylesheets().add("file:styles/DBMovements_Trigger.css");
 		triggerStage.show();
 		Helper_Methods.CenterStage(triggerStage);
 	}
